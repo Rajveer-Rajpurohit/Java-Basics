19df0f34fd370421e436f00c58d32f30d9ac9373
Rajveer-Rajpurohit <rajveerrajpurohit186@gmail.com>
2026-02-01 17:11:52 +0530

    Adding Project.java

---
+++ src/Project.java
@@ -0,0 +1,11 @@
+public class Project
+{
+    public static void main(String[]args)
+    {
+        int i = 10;
+        int j = 20;
+        int sum = i+j;
+        System.out.println("Sum Of i+j =" + sum);
+        System.out.println(args.length);
+    }
+}
